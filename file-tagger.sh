600cfe17a8cee245378b73c1eb9814e008a17cbd
Flühmann Tobias <tobias.fluehmann@swisscom.com>
2018-10-05 12:11:04 +0200

    added create and list

--- file-tagger.sh
+++ file-tagger.sh
@@ -1,30 +1,59 @@
 #! /bin/bash
 # Team: Matthias Sidler, Dominic Messmer, Tobias Flühmann
-DIR="~/.file-tagger/"
+dir=~/.file-tagger/
+counter_file=${dir}counter
+tags_desc='tags:'
+path_desc='path:'
+
 init() {
-  mkdir -p $DIR
+  mkdir -p $dir
 }
 
 list_files() {
-  files=`ls ~/.file-tagger/`
-  echo $files
-  PS3="Please select a file to show tags"
-  read -p "Input Selection:" -r line
-  path=${DIR}${line}
-  file=$(cat $path)
-  echo $file 
+  for filename in $dir*; do
+    basename=$(basename $filename)
+    printf "name: $basename - "
+    echo $(head -n 1 ${filename})
+  done
+  show_entry
+}
+
+show_entry(){
+  read -p "Enter number:" -r line
+  path=${dir}${line}
+  file_path=$(head -n 1 $path)
+  tags=$(tail -n 1 $path)
+  echo "$file_path" 
+  echo "$tags" 
+}
+
+# Read numbers from $dir and increase by one
+next_number(){
+  $(touch $counter_file)
+  count=$(cat $counter_file)
+  if [ -z "$count" ]; then
+    count=-1
+  fi
+  count="$(($count + 1))"
+  echo $count > $counter_file
+  echo $count
 }
 
 create_tag(){
-# Enter file path, enter tag
-sleep 0.1
+  filename=$(next_number)
+  # Enter file path, enter tag
+  read -p "Enter filepath:" -r line
+  echo "$path_desc $line"  >> ${dir}${filename}
+  read -p "Enter tags (comma separated):" -r line
+  echo "$tags_desc $line"  >> ${dir}${filename}
 }
+
 
 main() {
 
   init
   PS3='Please enter your choice: '
-  options=("option 1" "option 2" "option 3" "quit")
+  options=("option 1" "option 2" "option 3" "option 4" "quit")
   select opt in "${options[@]}"
   do
       case $opt in
@@ -33,10 +62,15 @@ main() {
               list_files
               ;;
           "option 2")
-              echo "delete tag"
+              echo "create tag"
+              create_tag
               ;;
           "option 3")
-              echo "you chose choice $REPLY which is $opt"
+              echo "add tag"
+              update
+              ;;
+          "option 4")
+              echo "delete tag"
               ;;
           "quit")
               break
