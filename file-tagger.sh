1a13299514ac60f98160f30b34556597f861ccb4
tobias fluehmann <t.fluehmann94@hotmail.com>
2018-10-05 13:49:18 +0200

    update tag, delete entry

--- file-tagger.sh
+++ file-tagger.sh
@@ -10,16 +10,17 @@ init() {
 }
 
 list_files() {
+  file_count=$(ls $dir | wc -l)
+  if [ "$file_count" -eq "0" ]; then echo "no tagged files found"; return; fi
   for filename in $dir*; do
     basename=$(basename $filename)
     printf "name: $basename - "
     echo $(head -n 1 ${filename})
   done
-  show_entry
 }
 
 show_entry(){
-  read -p "Enter number:" -r line
+  read -p "Enter number: " -r line
   path=${dir}${line}
   file_path=$(head -n 1 $path)
   tags=$(tail -n 1 $path)
@@ -31,9 +32,7 @@ show_entry(){
 next_number(){
   $(touch $counter_file)
   count=$(cat $counter_file)
-  if [ -z "$count" ]; then
-    count=-1
-  fi
+  if [ -z "$count" ]; then count=-1; fi
   count="$(($count + 1))"
   echo $count > $counter_file
   echo $count
@@ -42,24 +41,44 @@ next_number(){
 create_tag(){
   filename=$(next_number)
   # Enter file path, enter tag
-  read -p "Enter filepath:" -r line
+  read -p "Enter filepath: " -r line
   echo "$path_desc $line"  >> ${dir}${filename}
-  read -p "Enter tags (comma separated):" -r line
+  read -p "Enter tags (comma separated): " -r line
   echo "$tags_desc $line"  >> ${dir}${filename}
 }
 
+# so in dem stil:
+# printf ", $tag" >> $file_path
+update_tag(){
+  read -p "Enter number:" -r line
+  file_path=${dir}${line}
+  read -p "Enter new tag: " -r tag
+  printf ", $tag" >> $file_path
+}
+
+# tbd
+delete_tag(){
+sleep 0.1
+}
+
+delete_entry(){
+  read -p "Enter number:" -r line
+  file_path=${dir}${line}
+  rm $file_path
+}
 
 main() {
 
   init
   PS3='Please enter your choice: '
-  options=("option 1" "option 2" "option 3" "option 4" "quit")
+  options=("option 1" "option 2" "option 3" "option 4" "option 5" "quit")
   select opt in "${options[@]}"
   do
       case $opt in
           "option 1")
               echo "stored files:"
               list_files
+              show_entry
               ;;
           "option 2")
               echo "create tag"
@@ -67,10 +86,17 @@ main() {
               ;;
           "option 3")
               echo "add tag"
-              update
+              list_files
+              update_tag
               ;;
           "option 4")
               echo "delete tag"
+              delete_tag
+              ;;
+          "option 5")
+              echo "delete entry"
+              list_files
+              delete_entry
               ;;
           "quit")
               break
